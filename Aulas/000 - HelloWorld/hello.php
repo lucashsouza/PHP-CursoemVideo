1db14db2af231c671b830d4c79fbdb437f5a8cdc
Lucas Souza <lhenriquesouza00@gmail.com>
2018-09-30 22:40:45 -0300

    Add files via upload

---
+++ Aulas/000 - HelloWorld/hello.php
@@ -0,0 +1,10 @@
+<html>
+<head> <title> Hello World - PHP </title> </head>
+<body>
+
+<?php
+	echo "<h1 text-align=center> Hello World </h1>";
+?>
+
+</body>
+</html>
---
+++ Aulas/001 - Variables/var.php
@@ -0,0 +1,17 @@
+<html>
+<head> <title> Variables in PHP </title>
+<link rel='stylesheet' href='_css/estilo.css'> 
+</head>
+<body>
+	<div>
+		
+		<?php
+			$age = 18;
+			$name = 'Lucas';
+
+			echo "$name have $age Years old.";
+		?>
+
+	</div>
+</body>
+</html>
